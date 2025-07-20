33b3be168b5b40ffebece3def966c9b2ac434ffc
EV <EV@gmail.com>
2025-07-20 13:39:26 -0400

    one gene, parsed info

--- vn_per_gene/run_one_gene.py
+++ vn_per_gene/run_one_gene.py
@@ -8,9 +8,12 @@ script_directory = os.path.dirname(os.path.abspath(__file__))
 if __name__ == "__main__":
     print (f"Script dir: {script_directory}")
     infile = join(script_directory,"../vn_data/input/per_gene/ALS2.chr2.csv")
+    base_infile = basename(infile.replace(".csv",""))
+    outfile_parsed_INFO = join(script_directory,f"data/{base_infile}.parsed.INFO.csv")
+
     print(infile)
 
     one_gene_df = pd.read_csv(infile)
-    do_one_gene(df=one_gene_df)
+    do_one_gene(df=one_gene_df, outfile_parsed_INFO=outfile_parsed_INFO)
     pass
 
